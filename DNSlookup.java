30860c19193443d8b7925d7467b5ec1c4a1a1cdb
Sean Blair <seandibango@gmail.com>
2016-10-30 22:42:30 -0700

    Initial DNS query constructed with given fqdn, response received with same query id

--- DNSlookup.java
+++ DNSlookup.java
@@ -4,6 +4,7 @@ import java.util.*;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.util.Random;
+import java.util.regex.Pattern;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 /**
@@ -21,15 +22,25 @@ public class DNSlookup {
 	static final int MIN_PERMITTED_ARGUMENT_COUNT = 2;
 	static boolean tracingOn = false;
 	static InetAddress rootNameServer;
-	static int bufferSize = 512;
 	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) throws Exception {
-		String fqdn;
-		DNSResponse response; // Just to force compilation
+		DNSResponse response;
+		
 		int argCount = args.length;
+		
+		String fqdn;
+		String[] fqdnArray;
+		
+		DatagramSocket datagramSocket;
+		DatagramPacket packet;
+		int dnsPort = 53;
+		
+		int bufferSize = 512;
+		byte[] byteArrayBuffer;
+		int index = 0;
 		
 		if (argCount < 2 || argCount > 3) {
 			usage();
@@ -44,39 +55,83 @@ public class DNSlookup {
 		
 		// Start adding code here to initiate the lookup
 		
+		datagramSocket = new DatagramSocket();
+		byteArrayBuffer = new byte[bufferSize];
+
+// for comparing with given example query.
+//		byte[] exampleQuery = new byte[bufferSize];
+//		exampleQuery = Files.readAllBytes(Paths.get("DNSInitialQuery.bin"));
+//      byteArrayBuffer = exampleQuery;
 		
-		DatagramSocket datagramSocket = new DatagramSocket();
+		// Construct initial DNS Query
 		
-		byte[] byteArrayBuffer = new byte[bufferSize];
-		byteArrayBuffer = Files.readAllBytes(Paths.get("DNSInitialQuery.bin"));
-		
+		// assign a random number as queryId
+		// TODO look into a more complete implementation
 		Random r = new Random();
-		int queryId = r.nextInt(126);
-		byteArrayBuffer[0] = (byte) queryId;
-		queryId = r.nextInt(126);
-		byteArrayBuffer[1] = (byte) queryId;
+		int queryId = r.nextInt(126); 
+		byteArrayBuffer[index++] = (byte) queryId;
+		queryId = r.nextInt(255);
+		byteArrayBuffer[index++] = (byte) queryId;
 		
-        DatagramPacket packet = new DatagramPacket(byteArrayBuffer, byteArrayBuffer.length, rootNameServer, 53);
+		// set next 16 bits to 0
+		byteArrayBuffer[index++] = 0;
+		byteArrayBuffer[index++] = 0;
+		
+		//set Query Count (QDCOUNT) to 1
+		byteArrayBuffer[index++] = 0;
+		byteArrayBuffer[index++] = 1;
+		
+		// set Answer Count (ANCOUNT) to 0
+		byteArrayBuffer[index++] = 0;
+		byteArrayBuffer[index++] = 0;
+		
+		// set Name Server Records (NSCOUNT) to 0
+		byteArrayBuffer[index++] = 0;
+		byteArrayBuffer[index++] = 0;
+		
+		// set Additional Record Count (ARCOUNT) to 0
+		byteArrayBuffer[index++] = 0;
+		byteArrayBuffer[index++] = 0;
+		
+		// Start of QNAME
+		fqdnArray = fqdn.split(Pattern.quote("."));
+		
+		for (String part : fqdnArray) {
+			byteArrayBuffer[index++] = (byte) part.length();
+			for (char letter : part.toCharArray()) {
+				byteArrayBuffer[index++] = (byte) letter;
+			}
+		}
+		
+		// end with 0 (1 byte only) (indicates end of QNAME	
+		byteArrayBuffer[index++] = 0;
+		
+		// QTYPE
+		byteArrayBuffer[index++] = 0;
+		byteArrayBuffer[index++] = 1;
+		// QCLASS
+		byteArrayBuffer[index++] = 0;
+		byteArrayBuffer[index++] = 1;
+		
+		// send packet
+        packet = new DatagramPacket(byteArrayBuffer, byteArrayBuffer.length, rootNameServer, dnsPort); //
         datagramSocket.send(packet);
      
-        
-        
+        // receive response
+        // TODO implement time out check (5 seconds??)
         packet = new DatagramPacket(byteArrayBuffer, byteArrayBuffer.length);
         datagramSocket.receive(packet);
  
-
+        // not using this yet...
         response = new DNSResponse(byteArrayBuffer, bufferSize);
         
-        
-        //will have to decode here if encoded...
-        
+        // for viewing resulting dns response byte array.
         String received = Arrays.toString(byteArrayBuffer);
         System.out.println("Received this response from DNS server: \n" + received);
         
         datagramSocket.close();
 		
-		System.out.println("Hey dude, don't let me down..");
-		
+		System.out.println("Hey dude, it looks like it's working...");	
 	}
 
 	private static void usage() {
