9f52c2d6b2adb5ade1bf541a0bde7693907e5658
Gurjot Kheeva <gkheeva@gmail.com>
2016-11-05 23:35:05 -0700

    Exception classes commented.

--- GenericException.java
+++ GenericException.java
@@ -1,4 +1,7 @@
-
+/**
+ * Custom exception class corresponding to an error that was not in the specification
+ * for assignment, in this case we should handle it with a -4.
+ */
 public class GenericException extends Exception {
 
 }
--- NonExistentNameException.java
+++ NonExistentNameException.java
@@ -1,4 +1,7 @@
-
+/**
+ * Custom exception class for when a Name does not exist. We should handle 
+ * with a -1 as per assignment specifications.
+ */
 public class NonExistentNameException extends Exception {
 
 }
